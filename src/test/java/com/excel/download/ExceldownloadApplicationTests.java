cefce28cd9ddaa04d05d29a3985d4d710ce86f5e
LeeJinKyu <zlot1112@gmail.com>
2021-08-31 18:25:34 +0900

    Initial commit

---
+++ src/test/java/com/excel/download/ExceldownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excel.download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceldownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
